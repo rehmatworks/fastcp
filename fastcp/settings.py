c230d578e43b98b0bca0cc6e14409c0fdcc91261
Rehmat Alam <contact@rehmat.works>
2021-08-16 23:39:32 +0500

    Custom storage class

--- fastcp/settings.py
+++ fastcp/settings.py
@@ -169,7 +169,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'core.storage.WhiteNoiseStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
