d4d8f61cf0d83759e54e117db5be82bc59c94717
Rehmat Alam <contact@rehmat.works>
2021-08-16 23:25:51 +0500

    Static root

--- fastcp/settings.py
+++ fastcp/settings.py
@@ -144,9 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static/'),
-)
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
