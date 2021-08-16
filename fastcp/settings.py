e8018e4b71d84ce05263640a8aea04f978d3c5db
Rehmat Alam <contact@rehmat.works>
2021-08-17 02:20:03 +0500

    Using SQlite3

--- fastcp/settings.py
+++ fastcp/settings.py
@@ -122,22 +122,13 @@ WSGI_APPLICATION = 'fastcp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'OPTIONS': {
-                'read_default_file': BASE_DIR / 'fastcp.cnf',
-            },
-        }
-    }
+}
 
 
 # Password validation
