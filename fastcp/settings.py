b0077e7b656fe7ace292ac7d935772f3b2d9fc9b
Rehmat Alam <contact@rehmat.works>
2021-08-16 23:47:55 +0500

    Static dir

--- fastcp/settings.py
+++ fastcp/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'core.storage.WhiteNoiseStaticFilesStorage'
 
 # Default primary key field type
