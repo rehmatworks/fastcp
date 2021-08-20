0b185eb2e6949c1c10963adc794339f80c96460c
Rehmat Alam <contact@rehmat.works>
2021-08-21 01:47:45 +0500

    Attach domains

--- api/websites/serializers.py
+++ api/websites/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from core.models import Website
+from core.models import Website, Domain
+import validators
 
 
 class WebsiteSerializer(serializers.ModelSerializer):
@@ -13,10 +14,29 @@ class WebsiteSerializer(serializers.ModelSerializer):
         domains = list(filter(None, [domain.strip() for domain in value.strip().split(',')]))
         if len(domains) == 0:
             raise serializers.ValidationError({'domains': ['You have not provided any domains.']})
+        else:
+            for domain in domains:
+                # Check if domain is valid
+                if not validators.domain(domain):
+                    raise serializers.ValidationError({'domains': [f'{domain} is not a valid domain.']})
+                
+                # Ensure domain is unique
+                if Domain.objects.filter(domain=domain).count():
+                    raise serializers.ValidationError({'domains': [f'{domain} already exists in the database.']})
+        
+        return domains
 
     def create(self, validated_data):
-        self.validate_domains(self.context['request'].POST.get('domains'))
-        user = self.context['request'].user
+        request = self.context['request']
+        domains = request.POST.get('domains')
+        domains = self.validate_domains(domains)
+        user = request.user
         validated_data['user'] = user
         website = Website.objects.create(**validated_data)
+        
+        # Create domains
+        for domain in domains:
+            website.domains.create(
+                domain=domain
+            )
         return website
