6732c5c497016abae8fbaaccb55417f99b2984f2
Rehmat Alam <contact@rehmat.works>
2021-08-23 13:30:25 +0500

    Random password

--- core/utils/system.py
+++ core/utils/system.py
@@ -1,4 +1,6 @@
 from core.utils import filesystem
+import secrets
+import string
 import os, crypt
 from subprocess import (
     STDOUT, check_call, CalledProcessError
@@ -57,7 +59,14 @@ def delete_website(website: object):
     
     # Delete NGINX vhost files
     filesystem.delete_nginx_vhost(website)
-    
+   
+def rand_passwd(length: int = 20) -> str:
+    """Generate a random password.
+
+    Generate a random and strong password using secrets module.
+    """
+    alphabet = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(alphabet) for _ in range(length)) 
 
 def setup_user(user: object, password: str=None) -> bool:
     """Setup the user.
@@ -70,6 +79,10 @@ def setup_user(user: object, password: str=None) -> bool:
     Returns:
         bool: True on success and False otherwise.
     """
+    
+    if password is None:
+        password = rand_passwd(20)
+    
     # Create SSH user
     user_home = filesystem.get_user_path(user, exact=True)
     user_pass = crypt.crypt(password, 22)
