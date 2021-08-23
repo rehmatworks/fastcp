9464bb466c6dae5a4ee81d51264794bb387f7fd9
Rehmat Alam <contact@rehmat.works>
2021-08-23 18:56:11 +0500

    Restart services

--- core/signals.py
+++ core/signals.py
@@ -69,7 +69,7 @@ def restart_services_handler(sender=None, **kwargs):
     """Restarts services. Expects the service names as a comma-separated string."""
     services = kwargs.get('services').split(',')
     for service in services:
-        fcpsys.run_cmd(f'/usr/sbin/service {service} restart')
+        fcpsys.run_cmd(f'/usr/bin/systemctl restart {service}')
 
 restart_services.connect(restart_services_handler, dispatch_uid='restart-services')
 
@@ -78,6 +78,6 @@ def reload_services_handler(sender=None, **kwargs):
     """Reload services. Expects the service names as a comma-separated string."""
     services = kwargs.get('services').split(',')
     for service in services:
-        fcpsys.run_cmd(f'/usr/sbin/service {service} reload')
+        fcpsys.run_cmd(f'/usr/bin/systemctl restart {service}')
 
 reload_services.connect(reload_services_handler, dispatch_uid='reload-services')
