49b5e87dfba0e1b957f61e275a8bf38baa0e27b2
Rehmat Alam <contact@rehmat.works>
2021-08-25 20:17:36 +0500

    Try/except blocks to ensure that a fatal error doesnt prevent SSL activation for other websites

--- core/management/commands/activate-ssl.py
+++ core/management/commands/activate-ssl.py
@@ -13,18 +13,21 @@ class Command(BaseCommand):
         
         for website in websites:
             if website.needs_ssl() or ssl_expiring(website):
-                fcp = FastcpSsl()
-                activated = fcp.get_ssl(website)
+                try:
+                    fcp = FastcpSsl()
+                    activated = fcp.get_ssl(website)
 
-                if activated:
-                    self.stdout.write(self.style.SUCCESS(
-                        f'SSL certificate activated for website {website}'))
-                    website.has_ssl = True
-                    website.save()
-                    domains_updated.send(sender=website)
-                else:
-                    self.stdout.write(self.style.ERROR(
-                        f'SSL certificate cannot be activated for some or all domains for {website}'))
+                    if activated:
+                        self.stdout.write(self.style.SUCCESS(
+                            f'SSL certificate activated for website {website}'))
+                        website.has_ssl = True
+                        website.save()
+                        domains_updated.send(sender=website)
+                    else:
+                        self.stdout.write(self.style.ERROR(
+                            f'SSL certificate cannot be activated for some or all domains for {website}'))
+                except Exception as e:
+                    self.stdout.write(self.style.ERROR(f'{str(e)}'))
             else:
                 self.stdout.write(self.style.SUCCESS(
                     f'Website {website} does not need an SSL.'))
