4590b042679960378941c82fd739c831b5c20697
Rehmat Alam <contact@rehmat.works>
2021-09-17 16:25:39 +0500

    Use secret from env file

--- fastcp/settings.py
+++ fastcp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-swm^3n$0#i^x3uuh3hy&_h(%ud$a6qfo6#tnukvxmyem7j3x8='
+SECRET_KEY = os.environ.get('FASTCP_APP_SECRET', 'django-insecure-swm^3n$0#i^x3uuh3hy&_h(%ud$a6qfo6#tnukvxmyem7j3x8=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('IS_DEBUG') is not None
